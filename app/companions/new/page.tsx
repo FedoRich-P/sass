36ab01b4c16cc86306209d806b86ea3afd227940
Fedorich <panchev_fedor@mail.ru>
2025-05-25 23:51:47 +0300

    Base

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,5 +1,50 @@
-export default function NewCompanions() {
-  return <div>
-     New Companions Page
-  </div>
- }
+import {redirect} from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
+import {CompanionForm} from "@/components/Companion/CompanionForm";
+import {auth} from "@clerk/nextjs/server";
+// import {newCompanionPermissions} from "@/lib/actions/companion.actions";
+
+export default async function NewCompanions() {
+    const { userId } = await auth();
+    if(!userId) redirect('/sign-in');
+
+    // const canCreateCompanion = await newCompanionPermissions();
+
+    let a = 2
+
+    return (
+        <main className=" w-full md:w-3/4  lg:w-1/2 xl:w-2/5 items-center justify-center">
+            {a ? (
+                <article className="w-full gap-4 flex flex-col border p-5 rounded-md">
+                    <h1>Companion Builder</h1>
+
+                    <CompanionForm />
+                </article>
+            ) : (
+                <article className="companion-limit">
+                    <Image src="/images/limit.svg" alt="Companion limit reached" width={360} height={230} />
+                    <div className="cta-badge">
+                        Upgrade your plan
+                    </div>
+                    <h1>You’ve Reached Your Limit</h1>
+                    <p>You’ve reached your companion limit. Upgrade to create more companions and premium features.</p>
+                    <Link href="/subscription" className="btn-primary w-full justify-center" >
+                        Upgrade My Plan
+                    </Link>
+                </article>
+            )}
+            <article className="companion-limit">
+                <Image src="/images/limit.svg" alt="Companion limit reached" width={360} height={230}/>
+                <div className="cta-badge">
+                    Upgrade your plan
+                </div>
+                <h1>You’ve Reached Your Limit</h1>
+                <p>You’ve reached your companion limit. Upgrade to create more companions and premium features.</p>
+                <Link href="/subscription" className="btn-primary w-full justify-center">
+                    Upgrade My Plan
+                </Link>
+            </article>
+        </main>
+    )
+}
--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,5 +1,37 @@
-export default function Companions() {
-  return <div>
-      Companions Page
-  </div>
- }
+import {getAllCompanions} from "@/lib/actions/companion.actions";
+import {getSubjectColor} from "@/lib/utils";
+import CompanionCard from "@/components/Companion/CompanionCard";
+import SearchInput from "@/components/SearchInput";
+import {SubjectFilter} from "@/components/SubjectFilter";
+
+
+export default async function CompanionsLibrary ({ searchParams }: SearchParams) {
+    const filters = await searchParams;
+    const subject = filters.subject ? filters.subject : '';
+    const topic = filters.topic ? filters.topic : '';
+
+    const companions = await getAllCompanions({ subject, topic });
+
+    console.log(filters);
+
+    return (
+        <main>
+            <section className="flex justify-between gap-4 max-sm:flex-col">
+                <h1>Companion Library</h1>
+                <div className="grid grid-cols-2 gap-4">
+                    <SearchInput />
+                    <SubjectFilter />
+                </div>
+            </section>
+            <section className="companions-grid">
+                {companions.map((companion) => (
+                    <CompanionCard
+                        key={companion.id}
+                        {...companion}
+                        color={getSubjectColor(companion.subject)}
+                    />
+                ))}
+            </section>
+        </main>
+    )
+}
